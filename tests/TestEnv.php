475993f2c70dde5d0d692515653c501e08012531
yann <yann.kurdijaka@wanadoo.fr>
2013-03-22 09:29:17 +0100

    Correction du composer.json#2 + renommé

--- tests/TestEnv.php
+++ tests/TestEnv.php
@@ -3,4 +3,4 @@ error_reporting(E_ALL);
 
 include_once(__DIR__ . '/../vendor/autoload.php');
 
-include_once(__DIR__ . '/../src/classloader/ClassLoader.class.php');
+include_once(__DIR__ . '/../src/classloader/ClassLoader.php');
