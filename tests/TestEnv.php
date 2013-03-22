5bc71d752cf0946144811e7da198265eff501579
yann <yann.kurdijaka@wanadoo.fr>
2013-03-22 09:47:09 +0100

    Correction du composer.json#5

--- tests/TestEnv.php
+++ tests/TestEnv.php
@@ -3,4 +3,4 @@ error_reporting(E_ALL);
 
 include_once(__DIR__ . '/../vendor/autoload.php');
 
-include_once(__DIR__ . '/../src/classloader/ClassLoader.php');
+include_once(__DIR__ . '/../src/classloader/AlaroxClassLoader.php');
